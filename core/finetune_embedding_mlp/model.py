72c7e371d4cc7f3030226f6003bdb3561c508f48
chen <cshao676@gmail.com>
2024-09-06 22:28:57 +0200

    reset tag name

--- core/finetune_embedding_mlp/model.py
+++ core/finetune_embedding_mlp/model.py
@@ -1,6 +1,4 @@
 import os
-os.environ['PYTORCH_CUDA_ALLOC_CONF'] = 'expandable_segments:True'
-
 import sys
 import torch.nn.functional as F
 
@@ -177,8 +175,8 @@ class NCNClassifier(PreTrainedModel):
         pos_mask = (labels == 1)
         neg_mask = (labels == 0)
 
-        edge_pos = node_id[pos_mask].T
-        edge_neg = node_id[neg_mask].T
+        edge_pos = node_id[pos_mask]
+        edge_neg = node_id[neg_mask]
 
         edge_index = torch.tensor(self.edges, dtype=torch.long)
         adjmask = torch.ones_like(edge_index[0], dtype=torch.bool) # mask for adj
@@ -188,14 +186,15 @@ class NCNClassifier(PreTrainedModel):
         adj = SparseTensor.from_edge_index(tei, sparse_sizes=(self.data.num_nodes, self.data.num_nodes))
         adjmask[edge_pos] = 1
         adj = adj.to_symmetric()
-        outputs_1 = self.bert_encoder(input_ids=input_1,
-                                    attention_mask=attention_mask_1,
-                                    return_dict=return_dict,
-                                    output_hidden_states=True)
-        outputs_2 = self.bert_encoder(input_ids=input_2,
-                                    attention_mask=attention_mask_2,
-                                    return_dict=return_dict,
-                                    output_hidden_states=True)
+        with torch.no_grad():
+            outputs_1 = self.bert_encoder(input_ids=input_1,
+                                        attention_mask=attention_mask_1,
+                                        return_dict=return_dict,
+                                        output_hidden_states=True)
+            outputs_2 = self.bert_encoder(input_ids=input_2,
+                                        attention_mask=attention_mask_2,
+                                        return_dict=return_dict,
+                                        output_hidden_states=True)
         emb_1 = self.dropout(outputs_1['hidden_states'][-1])
         emb_2 = self.dropout(outputs_2['hidden_states'][-1])
         cls_token_emb_1 = emb_1.permute(1, 0, 2)[0]
@@ -212,8 +211,7 @@ class NCNClassifier(PreTrainedModel):
         neg_loss = -F.logsigmoid(-neg_outs).mean()
         loss = neg_loss + pos_loss
 
-        return TokenClassifierOutput(loss=loss, logits=torch.cat((pos_outs, neg_outs)))
-
+        return TokenClassifierOutput(loss=loss, logits=pos_outs+neg_outs)
 
 class NCNClaInfModel(PreTrainedModel):
     def __init__(self, model, emb, pred, data,inf_edges, dropout=0.0, seed=0, cla_bias=True, feat_shrink=''):
@@ -242,8 +240,8 @@ class NCNClaInfModel(PreTrainedModel):
         pos_mask = (labels == 1)
         neg_mask = (labels == 0)
 
-        edge_pos = node_id[pos_mask].T
-        edge_neg = node_id[neg_mask].T
+        edge_pos = node_id[pos_mask]
+        edge_neg = node_id[neg_mask]
 
         edge_index = torch.tensor(self.edges, dtype=torch.long)
         adjmask = torch.ones_like(edge_index[0], dtype=torch.bool)  # mask for adj
@@ -274,128 +272,3 @@ class NCNClaInfModel(PreTrainedModel):
         output = self.bert_classifier.predictor.multidomainforward(h, adj, node_id.permute(1, 0))
         loss = -F.logsigmoid(output).mean()
         return TokenClassifierOutput(loss=loss, logits=output)
-
-
-
-class GCNClassifier(PreTrainedModel):
-    def __init__(self, model, cfg, data, inf_edges, GCN_Model, dropout=0.0, seed=0,cla_bias=True, feat_shrink=''):
-        super().__init__(model.config)
-        self.bert_encoder = model
-        self.dropout = nn.Dropout(dropout)
-        self.edges = inf_edges
-        self.data = data
-        self.model = GCN_Model
-        init_random_state(seed)
-
-    def forward(self,
-                input_ids=None,
-                attention_mask=None,
-                labels=None,
-                token_type_ids=None,
-                node_id=None,
-                return_dict=None,
-                preds=None):
-        input_1 = input_ids[:, 0, :]
-        input_2 = input_ids[:, 1, :]
-        attention_mask_1 = attention_mask[:, 0, :]
-        attention_mask_2 = attention_mask[:, 1, :]
-        pos_mask = (labels == 1)
-        neg_mask = (labels == 0)
-
-        edge_pos = node_id[pos_mask].T
-        edge_neg = node_id[neg_mask].T
-
-        edge_index = torch.tensor(self.edges, dtype=torch.long)
-        adjmask = torch.ones_like(edge_index[0], dtype=torch.bool) # mask for adj
-        adjmask[edge_pos] = 0
-        tei = edge_index[:, adjmask]  # get the target edge index
-        # get the adj matrix
-        adj = SparseTensor.from_edge_index(tei, sparse_sizes=(self.data.num_nodes, self.data.num_nodes))
-        adjmask[edge_pos] = 1
-        adj = adj.to_symmetric()
-        row, col, _ = adj.coo()
-        batch_edge_index = torch.stack([col, row], dim=0)
-        outputs_1 = self.bert_encoder(input_ids=input_1,
-                                    attention_mask=attention_mask_1,
-                                    return_dict=return_dict,
-                                    output_hidden_states=True)
-        outputs_2 = self.bert_encoder(input_ids=input_2,
-                                    attention_mask=attention_mask_2,
-                                    return_dict=return_dict,
-                                    output_hidden_states=True)
-        emb_1 = self.dropout(outputs_1['hidden_states'][-1])
-        emb_2 = self.dropout(outputs_2['hidden_states'][-1])
-        cls_token_emb_1 = emb_1.permute(1, 0, 2)[0]
-        cls_token_emb_2 = emb_2.permute(1, 0, 2)[0]
-        batch_edge_index = batch_edge_index.to(cls_token_emb_1.device)
-        self.data.x = self.data.x.to(cls_token_emb_1.device)
-        node_id = node_id.to(cls_token_emb_1.device)
-        self.data.x[node_id[:,0]] = cls_token_emb_1
-        self.data.x[node_id[:,1]] = cls_token_emb_2
-        h = self.model.encoder(self.data.x, batch_edge_index).detach()
-        loss = self.model.recon_loss(h, edge_pos)
-        return TokenClassifierOutput(loss=loss, logits=h)
-
-class GCNClaInfModel(PreTrainedModel):
-    def __init__(self, model, emb, pred, data,inf_edges, dropout=0.0, seed=0, cla_bias=True, feat_shrink=''):
-        super().__init__(model.config)
-        self.bert_classifier = model
-        self.emb, self.pred = emb, pred
-        self.feat_shrink = feat_shrink
-        self.bert_encoder = model
-        self.dropout = nn.Dropout(dropout)
-        self.edges = inf_edges
-        self.data = data
-        init_random_state(seed)
-
-    def forward(self,
-                input_ids=None,
-                attention_mask=None,
-                labels=None,
-                token_type_ids=None,
-                node_id=None,
-                return_dict=None,
-                preds=None):
-        input_1 = input_ids[:, 0, :]
-        input_2 = input_ids[:, 1, :]
-        attention_mask_1 = attention_mask[:, 0, :]
-        attention_mask_2 = attention_mask[:, 1, :]
-        pos_mask = (labels == 1)
-        neg_mask = (labels == 0)
-
-        edge_pos = node_id[pos_mask].T
-        edge_neg = node_id[neg_mask].T
-
-        edge_index = torch.tensor(self.edges, dtype=torch.long)
-        adjmask = torch.ones_like(edge_index[0], dtype=torch.bool)  # mask for adj
-        adjmask[edge_pos] = 0
-        tei = edge_index[:, adjmask]  # get the target edge index
-        # get the adj matrix
-        adj = SparseTensor.from_edge_index(tei, sparse_sizes=(self.data.num_nodes, self.data.num_nodes))
-        adjmask[edge_pos] = 1
-        adj = adj.to_symmetric()
-        row, col, _ = adj.coo()
-        batch_edge_index = torch.stack([col, row], dim=0)
-        outputs_1 = self.bert_classifier.bert_encoder(input_ids=input_1,
-                                      attention_mask=attention_mask_1,
-                                      return_dict=return_dict,
-                                      output_hidden_states=True)
-        outputs_2 = self.bert_classifier.bert_encoder(input_ids=input_2,
-                                      attention_mask=attention_mask_2,
-                                      return_dict=return_dict,
-                                      output_hidden_states=True)
-        emb_1 = outputs_1['hidden_states'][-1]
-        emb_2 = outputs_2['hidden_states'][-1]
-        cls_token_emb_1 = emb_1.permute(1, 0, 2)[0]
-        cls_token_emb_2 = emb_2.permute(1, 0, 2)[0]
-        self.data.x = self.data.x.to(cls_token_emb_1.device)
-        adj = adj.to(cls_token_emb_1.device)
-        batch_edge_index = batch_edge_index.to(cls_token_emb_1.device)
-        node_id = node_id.to(cls_token_emb_1.device)
-        self.data.x[node_id[:, 0]] = cls_token_emb_1
-        self.data.x[node_id[:, 1]] = cls_token_emb_2
-        h = self.model.encoder(self.data.x, batch_edge_index).detach()
-        loss = self.model.recon_loss(h, edge_pos)
-        pred = self.model.decoder(h[node_id.T[0]], h[node_id.T[1]])
-        return TokenClassifierOutput(loss=loss, logits=pred)
-
